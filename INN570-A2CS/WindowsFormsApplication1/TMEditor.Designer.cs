d73d92f7c3b17b88794575a25a4d39ad1031f0e3
Matthew McMurtrie <matthew.mcmurtrie@student.qut.edu.au>
2014-09-29 12:31:54 +1000

    Initial

---
+++ INN570-A2CS/WindowsFormsApplication1/TMEditor.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class TMEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.layoutTable = new System.Windows.Forms.TableLayoutPanel();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.layoutTable.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Location = new System.Drawing.Point(3, 45);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.RowTemplate.Height = 24;
+            this.dataGrid.Size = new System.Drawing.Size(240, 150);
+            this.dataGrid.TabIndex = 0;
+            // 
+            // layoutTable
+            // 
+            this.layoutTable.ColumnCount = 2;
+            this.layoutTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.layoutTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.layoutTable.Controls.Add(this.dataGrid, 0, 1);
+            this.layoutTable.Controls.Add(this.btnExit, 1, 2);
+            this.layoutTable.Location = new System.Drawing.Point(12, 49);
+            this.layoutTable.Name = "layoutTable";
+            this.layoutTable.RowCount = 3;
+            this.layoutTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.93317F));
+            this.layoutTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 88.06683F));
+            this.layoutTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 49F));
+            this.layoutTable.Size = new System.Drawing.Size(669, 403);
+            this.layoutTable.TabIndex = 1;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExit.Location = new System.Drawing.Point(591, 377);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(719, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(175, 24);
+            this.openToolStripMenuItem.Text = "Open...";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // TMEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 477);
+            this.Controls.Add(this.layoutTable);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "TMEditor";
+            this.Text = "TMEditor";
+            this.Load += new System.EventHandler(this.TMEditor_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.layoutTable.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.TableLayoutPanel layoutTable;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ INN570-A2CS/WindowsFormsApplication1/TMEditor.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Resources;
+using System.Collections;
+
+namespace WindowsFormsApplication1
+{
+    public partial class TMEditor : Form
+    {
+        const string TableName = "ResourceTable";
+        const string col1Name = "col1";//needs meaningful name once
+        const string col2Name = "col2";//I know what goes into them
+        const int GridLineWidth = 1;
+        const int NumColumns = 2;
+        const string Separator = ";";
+
+        private DataTable resxTable;
+        public TMEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TMEditor_Load(object sender, EventArgs e)
+        {
+            //Initialize everything on startup
+            //Create a data table with two columns
+            this.resxTable = new DataTable(TableName);
+            this.resxTable.Columns.Add(new DataColumn(col1Name, Type.GetType("System.String")));
+            this.resxTable.Columns.Add(new DataColumn(col2Name, Type.GetType("System.DateTime")));
+            dataGrid.DataSource = resxTable;
+
+            //Set up a better looking table 
+            dataGrid.Columns[col1Name].Width = (dataGrid.Width - dataGrid.RowHeadersWidth - 2 * GridLineWidth) / NumColumns
+                  - GridLineWidth;
+            dataGrid.Columns[col2Name].Width = dataGrid.Columns[col1Name].Width; 
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Reset();
+            openFileDialog1.InitialDirectory = Directory.GetCurrentDirectory();
+            openFileDialog1.RestoreDirectory = false;
+            openFileDialog1.Filter = "Resource files (*.resx)|*.resx|All files (*.*)|*.*";
+            
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string target = openFileDialog1.FileName;
+                StreamReader MyStream = new StreamReader(openFileDialog1.FileName);
+                dataGrid.DataSource = null;
+                resxTable.Clear(); //Clear the existing table
+                dataGrid.DataSource = resxTable;
+
+                Dictionary<string, string> resourceMap = new Dictionary<string, string>();
+                ResXResourceReader resourceReader = new ResXResourceReader(target);
+                IDictionaryEnumerator dict = resourceReader.GetEnumerator();
+                foreach (DictionaryEntry d in resourceReader)
+                {
+                    resourceMap.Add(d.Key.ToString(), d.Value.ToString());
+                }
+                dataGrid.DataSource = resourceMap.ToArray();
+                dataGrid.Update();
+                dataGrid.Refresh();
+                
+                    
+
+                //Dictionary<string, string> resourceMap = new Dictionary<string, string>();
+                //string filename = "Your resource filepath";
+                //ResXResourceReader rsxr = new ResXResourceReader(filename);
+                //foreach (DictionaryEntry d in rsxr)
+                //{
+                //    resourceMap.Add(d.Key.ToString(), d.Value.ToString());
+                //}
+                //dataGridView1.DataSource = resourceMap.ToArray();
+
+                ///try
+                //{
+                //    while (true)
+                //    {
+                //        String MyLine = MyStream.ReadLine();
+                //        if (MyLine == null)
+                //        {
+                //            break;
+                //        }
+                //        else if (MyLine.Length != 0)
+                //        {
+                //            String[] fields = MyLine.Split(Separator.ToCharArray());
+                //            if (fields.GetLength(0) == 2)
+                //            {
+                //                resxTable.Rows.Add(resxTable.NewRow());
+                //                resxTable.Rows[resxTable.Rows.Count - 1][col1Name] =
+                //                 fields[0].Trim();
+                //                resxTable.Rows[resxTable.Rows.Count - 1][col2Name] =
+                //                fields[1].Trim();
+                //            }
+                //        }
+                //    }
+                //}
+                //catch (Exception ex)
+                //{
+                //    MessageBox.Show("Fatal Error" + ex.ToString());
+                //    Application.Exit();
+                //}
+            }
+        }
+
+        
+    }
+}
