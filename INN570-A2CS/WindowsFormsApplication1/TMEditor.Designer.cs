a5eb87e08c4fd8a030aa3b11972cd6516c491592
newage3680 <newage3680@gmail.com>
2014-10-19 18:00:05 +1000

    Saving tmx runs, now overwriting existing tmx

--- INN570-A2CS/WindowsFormsApplication1/TMEditor.Designer.cs
+++ INN570-A2CS/WindowsFormsApplication1/TMEditor.Designer.cs
@@ -31,16 +31,17 @@
             this.dataGrid = new System.Windows.Forms.DataGridView();
             this.layoutTable = new System.Windows.Forms.TableLayoutPanel();
             this.btnExit = new System.Windows.Forms.Button();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.lblResxCulture = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.cboCulture = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveTMToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
             this.layoutTable.SuspendLayout();
             this.menuStrip1.SuspendLayout();
@@ -54,21 +55,21 @@
             this.dataGrid.Margin = new System.Windows.Forms.Padding(2);
             this.dataGrid.Name = "dataGrid";
             this.dataGrid.RowTemplate.Height = 24;
-            this.dataGrid.Size = new System.Drawing.Size(307, 122);
+            this.dataGrid.Size = new System.Drawing.Size(477, 248);
             this.dataGrid.TabIndex = 0;
             // 
             // layoutTable
             // 
             this.layoutTable.ColumnCount = 3;
-            this.layoutTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 76.9802F));
-            this.layoutTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 23.0198F));
-            this.layoutTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 97F));
-            this.layoutTable.Controls.Add(this.btnExit, 1, 2);
+            this.layoutTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 84.514F));
+            this.layoutTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15.486F));
+            this.layoutTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
             this.layoutTable.Controls.Add(this.dataGrid, 0, 1);
             this.layoutTable.Controls.Add(this.lblResxCulture, 2, 0);
             this.layoutTable.Controls.Add(this.label2, 1, 0);
             this.layoutTable.Controls.Add(this.cboCulture, 2, 1);
             this.layoutTable.Controls.Add(this.label1, 1, 1);
+            this.layoutTable.Controls.Add(this.btnExit, 2, 2);
             this.layoutTable.Location = new System.Drawing.Point(9, 40);
             this.layoutTable.Margin = new System.Windows.Forms.Padding(2);
             this.layoutTable.Name = "layoutTable";
@@ -76,13 +77,13 @@
             this.layoutTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.93317F));
             this.layoutTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 88.06683F));
             this.layoutTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
-            this.layoutTable.Size = new System.Drawing.Size(502, 327);
+            this.layoutTable.Size = new System.Drawing.Size(708, 327);
             this.layoutTable.TabIndex = 1;
             // 
             // btnExit
             // 
             this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnExit.Location = new System.Drawing.Point(346, 306);
+            this.btnExit.Location = new System.Drawing.Point(650, 306);
             this.btnExit.Margin = new System.Windows.Forms.Padding(2);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(56, 19);
@@ -91,48 +92,10 @@
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
-            // menuStrip1
-            // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fileToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(539, 24);
-            this.menuStrip1.TabIndex = 2;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // fileToolStripMenuItem
-            // 
-            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.openToolStripMenuItem,
-            this.saveAsToolStripMenuItem});
-            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
-            this.fileToolStripMenuItem.Text = "File";
-            // 
-            // openToolStripMenuItem
-            // 
-            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
-            this.openToolStripMenuItem.Text = "Open...";
-            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
-            // 
-            // saveAsToolStripMenuItem
-            // 
-            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
-            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
-            this.saveAsToolStripMenuItem.Text = "Save as...";
-            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
-            // 
-            // openFileDialog1
-            // 
-            this.openFileDialog1.FileName = "openFileDialog1";
-            // 
             // lblResxCulture
             // 
             this.lblResxCulture.AutoSize = true;
-            this.lblResxCulture.Location = new System.Drawing.Point(407, 0);
+            this.lblResxCulture.Location = new System.Drawing.Point(610, 0);
             this.lblResxCulture.Name = "lblResxCulture";
             this.lblResxCulture.Size = new System.Drawing.Size(13, 13);
             this.lblResxCulture.TabIndex = 2;
@@ -141,7 +104,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(314, 0);
+            this.label2.Location = new System.Drawing.Point(516, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(85, 13);
             this.label2.TabIndex = 3;
@@ -150,7 +113,7 @@
             // cboCulture
             // 
             this.cboCulture.FormattingEnabled = true;
-            this.cboCulture.Location = new System.Drawing.Point(407, 37);
+            this.cboCulture.Location = new System.Drawing.Point(610, 37);
             this.cboCulture.Name = "cboCulture";
             this.cboCulture.Size = new System.Drawing.Size(92, 21);
             this.cboCulture.TabIndex = 4;
@@ -158,17 +121,63 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(314, 34);
+            this.label1.Location = new System.Drawing.Point(516, 34);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(76, 13);
             this.label1.TabIndex = 5;
             this.label1.Text = "Target culture:";
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(728, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.saveTMToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.openToolStripMenuItem.Text = "Open...";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.saveAsToolStripMenuItem.Text = "Save RESX...";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // saveTMToolStripMenuItem
+            // 
+            this.saveTMToolStripMenuItem.Name = "saveTMToolStripMenuItem";
+            this.saveTMToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.saveTMToolStripMenuItem.Text = "Save TM...";
+            this.saveTMToolStripMenuItem.Click += new System.EventHandler(this.saveTMToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
             // TMEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(539, 388);
+            this.ClientSize = new System.Drawing.Size(728, 388);
             this.Controls.Add(this.layoutTable);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -201,6 +210,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox cboCulture;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem saveTMToolStripMenuItem;
     }
 }
 
--- INN570-A2CS/WindowsFormsApplication1/TMEditor.cs
+++ INN570-A2CS/WindowsFormsApplication1/TMEditor.cs
@@ -11,6 +11,7 @@ using System.IO;
 using System.Resources;
 using System.Collections;
 using System.Globalization;
+using System.Xml;
 
 namespace WindowsFormsApplication1
 {
@@ -29,7 +30,7 @@ namespace WindowsFormsApplication1
             public string Key { get; set; }
             public string Value { get; set; }
         }
-        
+
 
         public TMEditor()
         {
@@ -51,8 +52,8 @@ namespace WindowsFormsApplication1
             String name;
             //Get a list of cultures - either all or installed
             list = CultureInfo.GetCultures(CultureTypes.AllCultures);
-            
-                //list = CultureInfo.GetCultures(CultureTypes.InstalledWin32Cultures);
+
+            //list = CultureInfo.GetCultures(CultureTypes.InstalledWin32Cultures);
 
             //Fill the combo boxes
             cboCulture.Items.Clear();
@@ -70,31 +71,34 @@ namespace WindowsFormsApplication1
 
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            BindingList<Entry> resxEntries = new BindingList<Entry>();
+
             openFileDialog1.Reset();
             openFileDialog1.InitialDirectory = Directory.GetCurrentDirectory();
             openFileDialog1.RestoreDirectory = false;
-            openFileDialog1.Filter = "Resource files (*.resx)|*.resx|All files (*.*)|*.*";
-
+            openFileDialog1.Filter = "Resource files (*.resx)|*.resx|TM files (*.tmx)|*.tmx|All files (*.*)|*.*";
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                string target = openFileDialog1.FileName;
-                string[] resxCulture = target.Split('.');
-                if(resxCulture.Length == 3){
-                    lblResxCulture.Text = resxCulture[1];
-                }
-                else lblResxCulture.Text = "Undefined";//filters based on filename
-                dataGrid.DataSource = null;
-                dataGrid.RowCount = 0;
-                dataGrid.ColumnCount = 0;
-                dataGrid.AutoGenerateColumns = true;
-
-                Dictionary<string, string> resourceMap = new Dictionary<string, string>();
-                ResXResourceReader resourceReader = new ResXResourceReader(target);
-                foreach (DictionaryEntry d in resourceReader)
+                if (openFileDialog1.FileName.Contains(".resx"))
                 {
-                    resxEntries.Add(new Entry() { Key = d.Key.ToString(), Value = d.Value.ToString() });
-                }
+                    BindingList<Entry> resxEntries = new BindingList<Entry>();
+                    string target = openFileDialog1.FileName;
+                    string[] resxCulture = target.Split('.');
+                    if (resxCulture.Length > 1)
+                    {
+                        lblResxCulture.Text = resxCulture[resxCulture.Length - 2];
+                    }
+                    else lblResxCulture.Text = "Undefined";//filters based on filename
+                    dataGrid.DataSource = null;
+                    dataGrid.RowCount = 0;
+                    dataGrid.ColumnCount = 0;
+                    dataGrid.AutoGenerateColumns = true;
+
+                    Dictionary<string, string> resourceMap = new Dictionary<string, string>();
+                    ResXResourceReader resourceReader = new ResXResourceReader(target);
+                    foreach (DictionaryEntry d in resourceReader)
+                    {
+                        resxEntries.Add(new Entry() { Key = d.Key.ToString(), Value = d.Value.ToString() });
+                    }
                     dataGrid.DataSource = resxEntries;
                     //Add third colum for user to select translations
                     var col3 = new DataGridViewTextBoxColumn();
@@ -102,9 +106,36 @@ namespace WindowsFormsApplication1
                     dataGrid.Columns[2].Name = "Target";
                     dataGrid.Update();
                     dataGrid.Refresh();
-                    
-            }
 
+                }
+                //Check a resx has been loaded
+                else if (dataGrid.Rows[0].Cells[0].Value.ToString() != null)
+                {
+                    //csv loaded
+                    XmlDocument doc = new XmlDocument();
+                    doc.Load(openFileDialog1.FileName);
+
+                    // }
+                    XmlNodeList nodelist = doc.SelectNodes("/tmx/body/tu/tuv");
+                    //for each entry in column 1
+                    foreach (DataGridViewRow row in dataGrid.Rows)
+                    {
+                        for (int i = 0; i < nodelist.Count; i++)
+                        {
+                            //find equivalent
+                            if (nodelist[i].SelectSingleNode("seg").InnerText == row.Cells[0].Value.ToString())
+                            {
+                                //Match found, set third column value
+                                row.Cells[2].Value = nodelist[i + 1].SelectSingleNode("seg").InnerText;
+                            }
+                        }
+                        //Check if no match was found
+                        if (row.Cells[2].Value == null)
+                            row.Cells[2].Value = "Undefined";
+                    }
+                    dataGrid.Refresh();
+                }
+            }
 
         }
 
@@ -114,23 +145,74 @@ namespace WindowsFormsApplication1
             saveFileDialog1.InitialDirectory = Directory.GetCurrentDirectory();
             saveFileDialog1.RestoreDirectory = false;
             var savetype = cboCulture.Text.Split(' ');
-            saveFileDialog1.Filter = string.Format("Resource files (*.{0}.resx)|*.{0}.resx", savetype[0]);
+            saveFileDialog1.Filter = string.Format("Resource files (*.{0}.resx)|*.{0}.resx| All files (*.*)|*.*", savetype[0]);
             if (saveFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 ResXResourceWriter writer = new ResXResourceWriter(saveFileDialog1.FileName);
                 dataGrid.EndEdit(); //Finalises latest entry
-                        foreach (DataGridViewRow row in dataGrid.Rows)
+                foreach (DataGridViewRow row in dataGrid.Rows)
+                {
+                    if ((row.Cells[0].Value != null && row.Cells[1].Value != null))
                     {
-                            if((row.Cells[0].Value.ToString() != null || row.Cells[2].Value.ToString() != null))
-                            {
-                                writer.AddResource(new ResXDataNode(row.Cells[0].Value.ToString(), row.Cells[2].Value.ToString()));
-                            }
-                            //TODO else error
-                      }
+                        writer.AddResource(new ResXDataNode(row.Cells[0].Value.ToString(), row.Cells[1].Value.ToString()));
+                    }
+                    //TODO else error
+                }
                 writer.Generate();
                 writer.Close();
             }
         }
 
+        private void saveTMToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Reset();
+            saveFileDialog1.InitialDirectory = Directory.GetCurrentDirectory();
+            saveFileDialog1.RestoreDirectory = false;
+            var savetype = cboCulture.Text.Split(' ');
+            saveFileDialog1.Filter = string.Format("TM files (*.tmx)|*.tmx| All files (*.*)|*.*", savetype[0]);
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                XmlDocument doc = new XmlDocument();
+                XmlElement tmx = doc.CreateElement("tmx");
+                XmlAttribute version = doc.CreateAttribute("version");
+                tmx.Attributes.Append(version);
+                tmx.Attributes[0].Value = "1.4";
+                //Code for header
+                XmlElement body = doc.CreateElement("body");
+                tmx.AppendChild(body);
+                int i = 0;
+                foreach (DataGridViewRow row in dataGrid.Rows)
+                {
+                    if (row.Cells[2].Value != null)
+                    {
+                        i++;
+                        XmlElement tu = doc.CreateElement("tu");
+                        XmlElement tuv = doc.CreateElement("tuv");
+                        XmlElement tuvTM = doc.CreateElement("tuv");
+                        XmlElement seg = doc.CreateElement("seg");
+                        XmlElement segTM = doc.CreateElement("seg");
+
+                        XmlAttribute tuid = doc.CreateAttribute("tuid");
+                        tu.Attributes.Append(tuid);
+                        tu.Attributes[0].Value = i.ToString();
+                        XmlAttribute lang = doc.CreateAttribute("xml:lang");
+                        tuv.Attributes.Append(lang);
+                        tuv.Attributes[0].Value = lblResxCulture.Text;
+                        tuvTM.Attributes.Append(lang);
+                        tuvTM.Attributes[0].Value = savetype[0];
+                        segTM.InnerText = row.Cells[2].Value.ToString();
+                        seg.InnerText = row.Cells[1].Value.ToString();
+
+                        body.AppendChild(tu);
+                        tu.AppendChild(tuv);
+                        tuv.AppendChild(seg);
+                        tu.AppendChild(tuvTM);
+                        tuvTM.AppendChild(segTM);
+                    }
+                }
+                doc.AppendChild(tmx);
+                doc.Save(saveFileDialog1.FileName);
+            }
+        }
     }
 }
